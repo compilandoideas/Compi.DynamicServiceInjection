cdd5517c9ec61e65d547d35a0c13baa3c8df24e4
Eduardo Muñoz Villagrán <emunoz@microexpertos.com>
2022-07-27 13:27:16 -0400

    a little of order

--- src/Compi.DynamicServiceInjection.Service/Program.cs
+++ src/Compi.DynamicServiceInjection.Service/Program.cs
@@ -34,12 +34,14 @@ namespace Compi.DynamicServiceInjection.Service
 
                     services.AddHostedService<Worker>();
 
+                    services.AddService(hostContext.Configuration);
 
-                    var serviceNamespace = hostContext.Configuration.GetSection("ServiceNamespace").Value;
 
-                    var assembly = Assembly.Load(serviceNamespace);
+                    //var serviceNamespace = hostContext.Configuration.GetSection("ServiceNamespace").Value;
 
-                    var typeFromConfigurationFile = assembly.GetType("Compi.DynamicServiceInjection.A.Service");
+                    //var assembly = Assembly.Load(serviceNamespace);
+
+                    //var typeFromConfigurationFile = assembly.GetType("Compi.DynamicServiceInjection.A.Service");
 
                     //var serviceWithType = Activator.CreateInstance(typeFromConfigurationFile) as IService;
                     //serviceWithType.Execute();
@@ -49,33 +51,6 @@ namespace Compi.DynamicServiceInjection.Service
 
 
 
-                    List<ConstructorInfo> ctors = assembly
-                        .GetTypes()
-                        .Select(x =>
-                        {
-                            Type type = x.IsGenericType ? x.MakeGenericType(typeof(IService).GenericTypeArguments) : x;
-                            return type.GetConstructors().Single();
-                        })
-                        .ToList();
-
-                        Func<IServiceProvider, object> func = provider =>
-                        {
-                            object current = null;
-
-                            foreach (ConstructorInfo ctor in ctors)
-                            {
-                                List<ParameterInfo> parameterInfos = ctor.GetParameters().ToList();
-
-                                object[] parameters = GetParameters(parameterInfos, current, provider);
-
-                                current = ctor.Invoke(parameters);
-                            }
-
-                            return current;
-                        };
-
-
-                    services.AddSingleton(typeof(IService), func);
 
                     //services.Scan(scan =>
                     //              scan
@@ -94,42 +69,6 @@ namespace Compi.DynamicServiceInjection.Service
             host.Run();
         }
 
-
-        private static object[] GetParameters(List<ParameterInfo> parameterInfos, object current, IServiceProvider provider)
-        {
-            var result = new object[parameterInfos.Count];
-
-            for (int i = 0; i < parameterInfos.Count; i++)
-            {
-                result[i] = GetParameter(parameterInfos[i], current, provider);
-            }
-
-            return result;
-        }
-
-        private static object GetParameter(ParameterInfo parameterInfo, object current, IServiceProvider provider)
-        {
-            Type parameterType = parameterInfo.ParameterType;
-
-            if (IsHandlerInterface(parameterType))
-                return current;
-
-            object service = provider.GetService(parameterType);
-            if (service != null)
-                return service;
-
-            throw new ArgumentException($"Type {parameterType} not found");
-        }
-
-
-        private static bool IsHandlerInterface(Type type)
-        {
-            if (!type.IsGenericType)
-                return false;
-
-            Type typeDefinition = type.GetGenericTypeDefinition();
-
-            return typeDefinition == typeof(IService);
-        }
     }
+      
 }
---
+++ src/Compi.DynamicServiceInjection.Service/ServiceRegistration.cs
@@ -0,0 +1,87 @@
+﻿using Compi.DynamicServiceInjection.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compi.DynamicServiceInjection.Service
+{
+    public static class ServiceRegistration
+    {
+        public static void AddService(this IServiceCollection services, IConfiguration configuration)
+        {
+
+            var serviceNamespace = configuration.GetSection("ServiceNamespace").Value;
+
+            var assembly = Assembly.Load(serviceNamespace);
+
+
+            ConstructorInfo ctor = assembly
+                .GetTypes()
+                .Select(x =>
+                {
+                    Type type = x.IsGenericType ? x.MakeGenericType(typeof(IService).GenericTypeArguments) : x;
+                    return type.GetConstructors().Single();
+                })
+                .First();
+
+            Func<IServiceProvider, object> func = provider =>
+            {
+                object current = null;
+
+                List<ParameterInfo> parameterInfos = ctor.GetParameters().ToList();
+
+                object[] parameters = GetParameters(parameterInfos, current, provider);
+
+                current = ctor.Invoke(parameters);
+
+
+                return current;
+            };
+
+
+            services.AddSingleton(typeof(IService), func);
+
+        }
+
+
+        private static object[] GetParameters(List<ParameterInfo> parameterInfos, object current, IServiceProvider provider)
+        {
+            var result = new object[parameterInfos.Count];
+
+            for (int i = 0; i < parameterInfos.Count; i++)
+            {
+                result[i] = GetParameter(parameterInfos[i], current, provider);
+            }
+
+            return result;
+        }
+
+        private static object GetParameter(ParameterInfo parameterInfo, object current, IServiceProvider provider)
+        {
+            Type parameterType = parameterInfo.ParameterType;
+
+            if (IsHandlerInterface(parameterType))
+                return current;
+
+            object service = provider.GetService(parameterType);
+            if (service != null)
+                return service;
+
+            throw new ArgumentException($"Type {parameterType} not found");
+        }
+
+
+        private static bool IsHandlerInterface(Type type)
+        {
+            if (!type.IsGenericType)
+                return false;
+
+            Type typeDefinition = type.GetGenericTypeDefinition();
+
+            return typeDefinition == typeof(IService);
+        }
+    }
+}
